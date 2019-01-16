3edc44cbf27e5493d3cb78caa998c37f867b421e
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2019-01-16 07:04:48 +0100

    ER - Functions

--- functions/singleFile/analysis.js
+++ functions/singleFile/analysis.js
@@ -34,12 +34,14 @@ function main(args) {
     var apiHost = args.functionsHost;
     var namespace = "_";
     var auth = args.functionsAuth;
-    if (args.own_debug === 'undefined'){
+      
+    if (args.own_debug == undefined){
         var own_debug = false;
     } else {
         var own_debug = args.own_debug;
     }
-
+    console.log("own_debug:",own_debug);
+      
     // generate api_key from auth
     var base64Auth = new Buffer(auth).toString('base64');
     var apiKey = "Basic " + base64Auth;
@@ -80,9 +82,9 @@ function main(args) {
                                              cloudantDocument = response;
 
                                              console.log("Entered Main Analysis Implementation");
-                                                console.log("own_debug:",own_debug);
+                                             
                                                 if (own_debug==false){
-                                                    
+                                                    console.log("own_debug:",own_debug);
                                                     if (cloudantDocument.hasOwnProperty("_id") &&
                                                         cloudantDocument.type == "image_db.image" &&
                                                         !cloudantDocument.hasOwnProperty("analysis") &&
@@ -303,8 +305,10 @@ function main(args) {
         // How to use this: https://javascript.info/promise-chaining
         console.log("own_debug:",own_debug);                                                    
         if (own_debug==false){
+            console.log("****** All steps to do ********");
             return p0(cloudantDocument).then(p8).then(p2).then(p3).then(p4).then(p5).then(p6).then(p7);
         } else {
+            console.log("****** Debug steps to do ********");
             return p0(cloudantDocument).then(p1).then(p2).then(p4);
         }
 
